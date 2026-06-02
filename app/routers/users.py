e05447cb5ea9bcead8d0d968d592f2b69d85e558
AdrianBarquinLIS <adrian.barquin@lisdatasolutions.com>
2026-06-02 16:28:03 +0200

    Corrección de filtrado routers/users.py

--- app/routers/users.py
+++ app/routers/users.py
@@ -2,9 +2,8 @@ from fastapi import APIRouter, status, Depends, HTTPException, Body
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, delete
 from sqlalchemy.orm import selectinload
+
 from schemas.user import UserOut, UserDeleteRequest, UserCreate, UserWithInactiveVehicles
-
-
 from database.session import get_db
 from models.user import User
 from models.vehicle import Vehicle
@@ -24,16 +23,15 @@ async def create_user(user: UserCreate,db: AsyncSession = Depends(get_db)):
     logger.info(f"Usuario creado con id {new_user.id}")
     return new_user
 
-
 @router.get("/active-with-inactive-vehicles", response_model=list[UserWithInactiveVehicles])
 async def get_active_users_with_inactive_vehicles(db: AsyncSession = Depends(get_db)):
     stmt = (
-        select(User).where(User.active.is_(True)).options(selectinload(User.vehicles.and_(Vehicle.active.is_(False))))
+        select(User).join 
+        (Vehicle, (Vehicle.user_id==User.id) & (Vehicle.active.is_(False)))
+        .where(User.active.is_(True)).options(selectinload(User.vehicles.and_(Vehicle.active.is_(False))))
     )
     result = await db.execute(stmt)
     users = result.scalars().all()
-
-
     return users
 
 @router.get("/", response_model=list[UserOut], summary="Listar usuarios", description="Todos los usuarios registrados, sin tener en cuenta si tienen vehiculo o no")
@@ -53,9 +51,6 @@ async def delete_users(
     result = await db.execute(stmt)
 
     if result.rowcount == 0:
-        raise HTTPException(status_code=404, detail="No se encontró ningún usuario con esos IDs")
-    
+        raise HTTPException(status_code=404, detail="No se encontró ningún usuario con esos IDs")   
     await db.commit()
-
     return {"deleted": result.rowcount}
-
